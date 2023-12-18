42bbd66f17ee78f37c745cc39d66873400e40afc
HmDol <vtwin321@naver.com>
2023-12-18 18:09:51 +0900

    moimlist 추가 및 member, guest 폴더 추가

--- crewz/src/Router.js
+++ crewz/src/Router.js
@@ -9,12 +9,15 @@ import ReviewAddModal from "./Component/review/ReviewAddModal"
 import AlbemHome from "./Component/Ablem/AlbemHome";
 import Moim from "./Moim";
 import App from "./App";
-
+import Main from "./Component/main";
+import MyPage from "./Component/member/MyPage";
+import MoimList from "./MoimList";
 
 class Router extends Component {
     render() {
         return (
             <Routes>
+                <Route exact path="/" element={<App />}></Route>
                 <Route path="/moim" element={<Moim />}>
                     <Route path="/moim/home" element={<Home />} />
                     <Route path="/moim/albem" element={<AlbemHome />} />
@@ -23,9 +26,11 @@ class Router extends Component {
                     <Route path="/moim/review/ReviewAddModal" element={<ReviewAddModal />} />
                     <Route path="/moim/review/ReviewEditModal" element={<ReviewEditModal />} />
                 </Route>
-                <Route exact path="/" element={<App />}></Route>
+                <Route path="/moimlist" element={<MoimList />}></Route>
                 <Route path="/temp" element={<Temp />} />
-
+                <Route exact path="/" element={<Main />} />
+                <Route path="/member/info" element={<MyPage />} />
+                <Route path="/temp" element={<Temp />} />
 
             </Routes>
         )
